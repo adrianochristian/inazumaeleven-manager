feb88fc664b7894a525957c303e6293df8d4002e
Adriano Christian <adriano.christian@atombank.com.br>
2024-05-28 23:36:24 -0300

    feat(score): adding score and time

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import time
 
 game_started = False
+match_time = 0
 
 def load():
     with open(( str(Path.home())+"/Downloads/IE1.csv"), 'r') as file:
@@ -23,6 +24,7 @@ def generate_team(players):
     team['DF'] = select_players(players, 'DF', 4)
     team['MF'] = select_players(players, 'MF', 4)
     team['FW'] = select_players(players, 'FW', 2)
+    team['Score'] = 0
     return team
 
 def select_players(players, position, count):
@@ -30,16 +32,20 @@ def select_players(players, position, count):
     selected_players = random.sample(list(filtered_players.keys()), count)
     return selected_players
 
-def match(blue, red):
-    global game_started
+def match(blue, red, player = None):
+    global game_started, match_time
     if game_started == False:
         player = touch(random.choice(list(blue['FW'])), blue)
         game_started = True
     
-    for i in range(10):
-        print("Minute " + str(i))
+    while match_time < 90:
+        print("Minute ", match_time)
+        match_time += 1
         attributes = players[player]
         player = action(player,attributes, blue, red)
+    
+    print("Full Time")
+    exit()
     
 def action(player,attributes,blue, red):
 
@@ -53,15 +59,16 @@ def action(player,attributes,blue, red):
         return touch(player, blue)
 
     if attributes['Position'] == 'FW':
-        return shoot(players[player], players[red['GK'][0]])
+        return shoot(players[player], players[red['GK'][0]], blue, red)
 
-def shoot(fw, gk):
+def shoot(fw, gk, blue, red):
     print(fw['Name'] + " shoots")
     time.sleep(1)
     if int(fw['Kick']) > int(gk['Guard']):
         print("Goal!")
-        exit()
-
+        blue['Score'] += 1
+        print("Score: ", blue['Score'], "-", red['Score'])
+        match(red, blue, gk['Name'])
     print("Saved by " + gk['Name'])
     gk['Guard'] = int(gk['Guard']) - 3
     return fw['Name']
@@ -70,7 +77,6 @@ def touch(player, team):
     print(player + " is with the ball")
     partner = random.choice([p for p in list(blue['DF'] + blue['MF'] + blue['FW']) if p != player])
     print(player + " passes to " + partner)
-    time.sleep(1)
     return partner
 
 players = load()
